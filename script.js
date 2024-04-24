29aa429226aaaff4edb2dc39ad0369ffbacc76ef
ARIELE SOUZA BITTENCOURT FARIAS <05850374000@senacrs.edu.br>
2024-04-24 12:19:52 -0300

    neuromente

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ $(document).ready(function() {
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var scrollRelato = document.getElementById("scrollRelato");
+    
+    scrollRelato.addEventListener("click", function(event) {
+        event.preventDefault(); // Impede o comportamento padrão do link
+        
+        var targetElement = document.getElementById("part2"); // Obtém o elemento alvo
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop, // Rolagem até a posição do elemento alvo
+                behavior: "smooth" // Rola suavemente
+            });
+        }
+    });
+});
